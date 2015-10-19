43748e9630a34572d333f7827ea54ac6b09d9d76
dbykovskyy <dbykovskyy@trulia.com>
2015-10-18 22:53:29 -0700

    - added untracked files to git

---
+++ app/src/main/java/com/example/dbykovskyy/sumofus/utils/Config.java
@@ -0,0 +1,14 @@
+package com.example.dbykovskyy.sumofus.utils;
+
+/**
+ * Created by dbykovskyy on 10/17/15.
+ */
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCjc330rSuNzWpIBaEdeEwqGB_OZAv-Vn8";
+
+
+}
