f74484c28bd9358c56a1b0ca3dbe112cde80875f
dbykovskyy <dbykovskyy@trulia.com>
2015-10-20 18:07:00 -0700

    - Fixed issue with hiding action bar CampaignDetailActivity - Base implementation of Cardio for donation

--- app/src/main/java/com/example/dbykovskyy/sumofus/activity/CampaignDetailActivity.java
+++ app/src/main/java/com/example/dbykovskyy/sumofus/activity/CampaignDetailActivity.java
@@ -40,8 +40,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class CampaignDetailActivity extends YouTubeBaseActivity {
-    private ShareActionProvider miShareAction;
+public class CampaignDetailActivity extends AppCompatActivity {
 
     private static final int TAKE_PHOTO_CODE = 1;
     private static final int PICK_PHOTO_CODE = 2;
--- app/src/main/java/com/example/dbykovskyy/sumofus/activity/CampaignsActivity.java
+++ app/src/main/java/com/example/dbykovskyy/sumofus/activity/CampaignsActivity.java
@@ -1,7 +1,9 @@
 package com.example.dbykovskyy.sumofus.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -26,6 +28,9 @@ import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
 
 public class CampaignsActivity extends AppCompatActivity {
     String imageUrl; // = "http://sumofus.org/wp-content/uploads/2015/10/38b73ede-6a13-433c-9c76-a567ccfea8b1.jpg";
@@ -69,19 +74,22 @@ public class CampaignsActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            switch(item.getItemId()) {
 
-        if(id==R.id.user_profile){
+                case R.id.action_account:
+                {
+                    Intent i = new Intent(this, UserProfileActivity.class);
+                    startActivity(i);
+                }
+                break;
+                case  R.id.action_donate:
+                {
+                    onScanPress();
+                }
+                break;
+            }
 
-            Intent i = new Intent(this, UserProfileActivity.class);
-            startActivity(i);
-
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -138,4 +146,54 @@ public class CampaignsActivity extends AppCompatActivity {
         startActivityForResult(i, 0);
 
     }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityResult(resultCode, resultCode, data);
+
+        if (resultCode == 777) {
+            String resultDisplayStr;
+            if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
+                CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+
+                // Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
+                resultDisplayStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+
+                // Do something with the raw number, e.g.:
+                // myService.setCardNumber( scanResult.cardNumber );
+
+                if (scanResult.isExpiryValid()) {
+                    resultDisplayStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n";
+                }
+
+                if (scanResult.cvv != null) {
+                    // Never log or display a CVV
+                    resultDisplayStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+                }
+
+                if (scanResult.postalCode != null) {
+                    resultDisplayStr += "Postal Code: " + scanResult.postalCode + "\n";
+                }
+            }
+            else {
+                resultDisplayStr = "Scan was canceled.";
+            }
+            // do something with resultDisplayStr, maybe display it in a textView
+            // resultTextView.setText(resultStr);
+        }
+        // else handle other activity results
+    }
+
+    public void onScanPress() {
+        Intent scanIntent = new Intent(this, CardIOActivity.class);
+
+        // customize these values to suit your needs.
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false); // default: false
+
+        // MY_SCAN_REQUEST_CODE is arbitrary and is only used within this activity.
+        startActivityForResult(scanIntent, 777);
+    }
+
 }
